6b48a83262ea63e819e074687497702966998104
namdlefhtes <namdlefhtes@gmail.com>
2022-03-07 17:00:32 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 
 import {buildPage} from './build-page.js';
 import { dropDown } from './drop-down.js';
+import {Background} from './assets/bg.png';
+
+const bg = new Image();
+bg.src = Background;
+
 
 buildPage();
 dropDown();
